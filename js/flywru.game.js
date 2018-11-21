5851832e525429ae73d090eec9e8082839656480
DocMadness <tdocmadness@gmail.com>
2018-11-21 19:06:22 +0200

    #6. Добавление пустых функций.

--- js/flywru.game.js
+++ js/flywru.game.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
   $("#gamefield").load("gamefield.html"); 
 });
+
+function maincontrolbutton_up() {
+	alert('Up');
+}
+
+function maincontrolbutton_left() {
+	alert('Left');
+}
+
+function maincontrolbutton_right() {
+	alert('Right');
+}
+
+function maincontrolbutton_down() {
+	alert('Down');
+}
